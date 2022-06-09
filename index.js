8c00d5d2d40967f65b934f03539af64fb314490d
Randiel Zoquier <rzstarmine@gmail.com>
2022-06-09 11:38:22 -0400

    Version 2.0
    Added an upload button in the controller and fixed redirect on the root directory

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ const express = require('express');
 const {Server} = require('socket.io');
 const cors = require('cors');
 const fs = require("fs");
+const multer = require("multer");
 
 const app = express();
 const server = http.createServer(app).listen(8080);
 
 let controller;
-let fileNames = fs.readdirSync('client/sounds/');
+let stor = multer.diskStorage({destination: (req,file,cb)=>{
+    cb(null, "client/sounds/");
 
+},
+filename: (req,file,cb)=>{
+    const {originalname} = file;
+    console.log(originalname);
+    cb(null, originalname);
+}});
+let upload = multer({storage: stor});
 // console.log(fileNames);
 
 // const io = new Server(server, {transports: ['websocket']});
 const io = new Server(server);
 app.use( "/controller", express.static(__dirname +"/controller/"));
-app.use("/client", express.static(__dirname +"/client/"));
+app.use("/", express.static(__dirname +"/client/"));
 
 app.get("/", (req,res)=>{
-    res.writeHead(301, { Location: '/client/' });
+    res.sendFile(__dirname +"/client/index.html");
 });
 
 app.get("/filenames/", (req, res)=>{
+    let fileNames = fs.readdirSync('client/sounds/');
     res.send(fileNames);
 });
 
@@ -31,6 +41,11 @@ app.get("/controller", (req,res)=>{
 
 app.get("/client", (req, res)=>{
     res.sendFile(__dirname +"/client/index.html");
+});
+
+app.post("/upload", upload.single('upload'),(req,res)=>{
+    res.write('<p> file uploaded successfully, <a href="../controller"> click here</a> to return to the controller </p>');
+    res.end();  
 });
 
 io.on("connection", (socket)=>{
